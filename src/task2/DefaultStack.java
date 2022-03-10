987a18928b7dd652f070ac5201e6fd85c6516a48
Mher <mherbadalyan@gmail.com>
2022-03-10 22:05:14 +0400

    final

--- src/task2/DefaultStack.java
+++ src/task2/DefaultStack.java
@@ -1,17 +1,16 @@
 package task2;
 
 public class DefaultStack implements Stack {
-    final int MAX_SIZE = 5;
-    int[] nums = new int[MAX_SIZE];
-    int index = 0;
+    private final int MAX_SIZE = 5;
+    private final int[] nums = new int[MAX_SIZE];
+    private int index = 0;
 
     @Override
     public void push(int val) {
         if (index == MAX_SIZE) {
             throw new StackIndexOutOfBoundsException();
-        } else {
-            nums[index++] = val;
         }
+        nums[index++] = val;
     }
 
     @Override
@@ -19,10 +18,9 @@ public class DefaultStack implements Stack {
         int temp;
         if (index == 0) {
             throw new EmptyStackException();
-        } else {
-            temp = nums[--index];
-            nums[index] = 0;
-            return temp;
         }
+        temp = nums[--index];
+        nums[index] = 0;
+        return temp;
     }
 }
